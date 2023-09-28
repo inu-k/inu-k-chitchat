57b60c42083ef05677bf0fee2b0909a84247d2c7
inu <inu6411e@gmail.com>
2023-09-29 00:22:15 +0900

    modify test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import App from './App.js';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders See threads link', () => {
+  render(<React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>);
+  const linkElement = screen.getByText(/See Threads/i);
   expect(linkElement).toBeInTheDocument();
 });
