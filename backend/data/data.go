e9d89af04c9176d06985326a4325625730a29858
inu-k <inu6411e@gmail.com>
2023-10-03 23:44:22 +0900

    change init

--- backend/data/data.go
+++ backend/data/data.go
@@ -14,11 +14,14 @@ var Db *sql.DB
 func init() {
 	var err error
 	err = godotenv.Load()
+	var dbUser, dbPassword string
 	if err != nil {
-		panic(err)
+		dbUser = "postgres"
+		dbPassword = "postgres"
+	} else {
+		dbUser = os.Getenv("DB_USER")
+		dbPassword = os.Getenv("DB_PASSWORD")
 	}
-	dbUser := os.Getenv("DB_USER")
-	dbPassword := os.Getenv("DB_PASSWORD")
 	Db, err = sql.Open("postgres", fmt.Sprintf("user=%s password=%s dbname=chitchat sslmode=disable", dbUser, dbPassword))
 	if err != nil {
 		panic(err)
